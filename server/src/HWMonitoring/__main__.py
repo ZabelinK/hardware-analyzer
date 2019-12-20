4d951c51b22e45bd8939ed74f9fef02d350a841e
Kirill Zabelin <Kirill.Zabelin@dell.com>
2019-12-20 14:44:26 +0300

    Polling update state of machine

--- server/src/HWMonitoring/__main__.py
+++ server/src/HWMonitoring/__main__.py
@@ -5,7 +5,24 @@ import connexion
 from HWMonitoring import encoder
 from HWMonitoring import db
 
+from HWMonitoring.models.machine import Machine
+from HWMonitoring.models.statistic import Statistic
+
 from os import system
+import time
+
+MAX_TIME = 900
+
+def machine_update()
+    cur_time = time.time()
+    machines = Machine.query.all()
+    for machine in machines:
+        statistic = Statistic.query.filter_by(machine_id_=machine.id_).first()
+        if cur_time - statistic.timestamp_ > MAX_TIME:
+            machine.status_ = "DOWN"
+        else:
+            machine.status_ = "RUNNING"
+    db.connection.commit()
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
